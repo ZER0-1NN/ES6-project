b72e428839b61f0692a680354d84a7c95d5567a6
[ZER0.1] <53665362+ZER0-1NN@users.noreply.github.com>
2020-08-07 00:23:35 -0400

    Update exercicio2-module1.js

--- exercicio2-module1.js
+++ exercicio2-module1.js
@@ -2,10 +2,31 @@ const usuarios = [
     { nome: 'Diego', idade: 23, empresa: 'Rocketseat' },
     { nome: 'Gabriel', idade: 15, empresa: 'Rocketseat' },
     { nome: 'Lucas', idade: 30, empresa: 'Facebook' },
+    { nome: 'Luis', idade: 17, empresa: 'Google' },
 ];
 
-const idades = usuarios.map(function(item) {
-    return item.idade;
+// 2.1 Utilizando o map
+
+const idades = usuarios.map(user => {
+    return user.idade;
 });
 
-console.log(idades);
+// 2.2 Utilizando o filter
+
+const usuariosRocketseat = usuarios.filter(user => {
+    return user.empresa === 'Rocketseat' && user.idade >= 18
+});
+
+// 2.3 Utilizando o find
+
+const usuariosGoogle = usuarios.filter(user => {
+    return user.empresa === 'Google'
+});
+
+// 2.4 Unindo operações
+
+const resultado = usuarios.map(user => {
+    return ({...user, idade: user.idade*2})
+}).filter(user => {
+    return user.idade <= 50
+});
