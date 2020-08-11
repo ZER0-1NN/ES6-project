a86783e0e0d68a667b1d59680107f3f8e3815f97
Luis <luis.gsn@outlook.com>
2020-08-10 22:37:14 -0400

    foda

---
+++ Project/src/main.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+class Api {
+    static async getUserInfo(username) {
+        const response = await axios.get(`https://api.github.com/${username}`);
+        console.log(responde);
+    }
+}
