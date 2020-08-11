62b7942b0c58d3f8d206f14dbb9303e9f25a6600
Luis <luis.gsn@outlook.com>
2020-08-10 22:39:42 -0400

    dahora

--- Project/src/main.js
+++ Project/src/main.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 class Api {
     static async getUserInfo(username) {
-        const response = await axios.get(`https://api.github.com/${username}`);
-        console.log(responde);
+        const response = await axios.get(`https://api.github.com/users/${username}`);
+        console.log(response);
     }
 }
+
+Api.getUserInfo('ZER0-1NN')
